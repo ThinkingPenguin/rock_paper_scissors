78c30f231bd67472ff66b33a6f4c19ec4458a32b
ThinkingPenguin <134267649+ThinkingPenguin@users.noreply.github.com>
2023-06-14 23:01:09 +0200

    Finished to update js files by adding the score and give an alert when someone arrived at 5 points

--- scripts.js
+++ scripts.js
@@ -1,18 +1,64 @@
 let counterPlayer = 0;
 let counterComputer = 0;
-/*
-function getPlayerChoice() {
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            let playerSelection = e.target.getAttribute('data-class');
-            return playerSelection;
-        })
-        
-    });
-    
-}*/
 
+// Initializing variable for sentence part
+const containerSentence = document.querySelector('.sentence-container');
+const sentence = document.createElement('p');
+sentence.classList.add('sentence');
+sentence.style.fontSize = '50px';
+
+// initializing variable to display score for human
+const human = document.querySelector('.left-side');
+const humanScore = document.createElement('p');
+humanScore.classList.add('human-score');
+humanScore.style.fontSize = '50px';
+
+// initializing variable to display score for computer
+const computer = document.querySelector('.right-side');
+const computerScore = document.createElement('p');
+computerScore.classList.add('computer-score');
+computerScore.style.fontSize = '50px';
+
+// initializing variable to display who won point
+const whoWonContainer = document.querySelector('.who-wins-container');
+const whoWon = document.createElement('p');
+whoWon.classList.add('who-won');
+whoWon.style.fontSize = '50px';
+
+function countPoint(){
+    if (counterPlayer > counterComputer) {
+        whoWonContainer.appendChild(whoWon);
+        whoWon.textContent = `You won! Player : ${counterPlayer}  ||   
+        Computer : ${counterComputer}`;
+        
+        
+    } else if (counterComputer > counterPlayer){
+        whoWonContainer.appendChild(whoWon);
+        whoWon.textContent = `You lose! Player : ${counterPlayer} || 
+        Computer : ${counterComputer}`;
+        
+    } else {
+        whoWonContainer.appendChild(whoWon);
+        whoWon.textContent = `It's a draw! Player : ${counterPlayer} || 
+        Computer : ${counterComputer}`;
+        
+    }
+    human.appendChild(humanScore);
+    humanScore.textContent = `${counterPlayer}`;
+
+    computer.appendChild(computerScore);
+    computerScore.textContent = `${counterComputer}`;
+
+}
+
+
+function showChoices(playerChoice, computerChoice){
+    const sentenceContent = `Human choose ${playerChoice} and computer 
+    choose ${computerChoice}`;
+    containerSentence.appendChild(sentence);
+    return sentence.textContent = sentenceContent;
+
+}
 
 
 function getComputerChoice(){
@@ -57,9 +103,18 @@ function playRound(playerSelection, computerSelection){
         }
     }
 
-    console.log(playerSelection, computerSelection);
-    console.log(`Player : ${counterPlayer}, Computer : ${counterComputer}`)
-
+    showChoices(playerSelection, computerSelection);
+    countPoint();
+    if (counterComputer >= 5 && counterComputer > counterPlayer) {
+        alert('You lose !');
+        counterComputer = 0;
+        counterPlayer = 0
+    } else if (counterPlayer >= 5 && counterPlayer > counterComputer){
+        alert('You won !');
+        counterComputer = 0;
+        counterPlayer = 0
+    }
+    
 }
 
 function game(){
@@ -73,33 +128,11 @@ function game(){
     })
     
 });
-    
-    let i = 0;
-    //console.log(playRound(playerSelection, computerSelection));
-    
+        
 }
 
-/*
-for (let index = 0; index < 5; index++) {
-    game();
-    
-}
+game();
 
-if (counterPlayer > counterComputer) {
-    console.log(`You won! Player : ${counterPlayer}, 
-    Computer : ${counterComputer}`);
-    
-    
-} else if (counterComputer > counterPlayer){
-    console.log(`You lose! Computer : ${counterComputer}, 
-    Player : ${counterPlayer}`);
-    
-} else {
-    console.log(`It's a draw! Computer : ${counterComputer}, 
-    Player : ${counterPlayer}`);
-    
-}
-*/
 
 
 
